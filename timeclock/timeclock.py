f803cb03074209f326744a4b1ec2ddacba4afe67
Nanrou <kkkcomkkk@qq.com>
2017-12-08 11:34:53 +0800

    add time clock

---
+++ timeclock/timeclock.py
@@ -0,0 +1,27 @@
+import time
+import logging
+from functools import wraps
+
+Logger = logging.getLogger('time-logger')
+
+
+def time_clock(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        Logger.debug('start {fun_name}({fun_args}{fun_kwargs})'
+                     .format(fun_name=func.__name__, fun_args=args or '', fun_kwargs=kwargs or ''))
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        Logger.debug('--{}-- cost: {}'.format(func.__name__, my_round(time.time() - start_time)))
+        return res
+    return wrapper
+
+
+def my_round(num):
+    if num < 60:
+        n = round(num, 3)
+        if n < 1:
+            return 'biu~'
+        return '{} s'.format(n)
+    elif num < 3600:
+        return '{} min {} s'.format(int(num/60), round(num % 60, 2))
